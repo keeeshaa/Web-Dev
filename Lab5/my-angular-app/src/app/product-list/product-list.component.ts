3cb46edc9f51379523eeb4332697a0826047e02d
Kesha <kesha@gmail.com>
2025-03-30 22:42:33 +0500

    lab5 updated

---
+++ Lab5/my-angular-app/src/app/product-list/product-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+import { Product } from '../model/product.model';
+import {ProductItemComponent} from '../product-item/product-item.component';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  imports: [
+    ProductItemComponent, CommonModule
+  ],
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  @Input() products: Product[] = [];
+}
